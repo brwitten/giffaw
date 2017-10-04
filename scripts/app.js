97d0032edd74abad51e5081b538575fad135f0c0
brwitten <bethrwitten@gmail.com>
2017-10-04 09:42:14 -0700

    removing console logs that aren't needed

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,24 @@
 $(document).on("ready", function(){
-
   $.ajax({
     method: "GET",
     url: "http://api.giphy.com/v1/gifs/search",
     data: $("form").serialize(),
     success: onSuccess,
-    error: onError,
-    key: "NcLhtT9vwfMS0UJ9TtZYwjzkq0KpyH6q"
-
+    error: onError
   })
-
 });
 
 function onSuccess(json) {
   console.log("success got data");
-  console.log(json.data);
-  console.log(json.length);
   for (var i = 0; i < json.data.length; i++) {
     $(".gif-gallery").append('<img src="'+json.data[i].images.fixed_height.url+'">');
     console.log(json.data[i].images.fixed_height.url);
   }
-}
-
+};
 
 function onError(xhr, status, errorThrown) {
 	alert("Sorry, there was a problem!");
 	console.log("Error: " + errorThrown);
 	console.log("Status: " + status);
 	console.dir(xhr);
-}
+};
